e4fb3ff1e757710886a57739fdb64ac4c3d924be
pseudo-wildfox <55625929+pseudo-wildfox@users.noreply.github.com>
2019-09-21 19:20:47 +0400

    Update config.php

--- vk/config.php
+++ vk/config.php
@@ -1,1 +1,8 @@
-
+<?php
+    $album = 'https://vk.com/album-186759710_266769713';
+    $res = parse_url($album);
+    $path = substr($res['path'],6);
+    $arr = explode('_', $path);
+    $owner_id = $arr[0];
+    $album_id = $arr[1];    
+?>
